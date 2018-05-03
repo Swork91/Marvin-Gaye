2e8a9c48eca71284858e38c45ce2fe445c6fccc7
Swork91 <chicagomike999@gmail.com>
2018-05-03 14:18:17 -0400

    Almost at version 0.0
    Added the feel of it being a game, but I'm still brainstorming some
    ideas as what I want to do with it. Also reading up more on what the
    usual accepted syntax on things is regarding Python.

--- src/Marvin/Marvin.py
+++ src/Marvin/Marvin.py
@@ -15,38 +15,119 @@ class Creature:
         self.damage = damage
         self.jazz = jazz # magic damage
         
+    def __repr__(self):
+        return "Creature('{}', {}, {}, {})".format(self.name, self.albums, self.jazz, self.damage)
+
+    def __str__(self):
+        return " Name: {}\n Albums: {}\n Damage: {}".format(self.name, self.albums, self.damage)
+
     def attack(self, Creature):
-        print('{} attacks you for {}.'.format(Creature.name, self.damage))
+        print('{} attacks you for {}.'.format(self.name, self.damage))
+        Creature.albums-=self.damage
+        
+    def checkIfDead(self):
+        if self.albums<=0:
+            print("{} has died".format(self.name))
         
 class Marvin(Creature):
     'the man himself'
     def attack(self, Creature):
         print('You attack {} for {}.'.format(Creature.name, self.damage))
+        Creature.albums-=self.damage
         
     def heal(self):
         print('You heal for {}.'.format(self.jazz))
+        self.albums+=self.jazz
         
+marvin = Marvin('Marvin', 17, 6, 5) # Create marvin right away since hes kinda important to everything? 
+
+'''
+combat handlers
+'''
+def combatOneOnOne(Creature1):
+    playerChoice = raw_input('Enter things: ')
+    while (playerChoice != '0'): 
+        if (playerChoice=='1'): # attack target
+            marvin.attack(Creature1)
+            Creature1.attack(marvin)
+            Creature1.checkIfDead()
+        if (playerChoice=='2'): # Heal self
+            marvin.heal()
+            Creature1.attack(marvin)
+        if (playerChoice=='3'): # inspect self
+            print(marvin)
+        if (playerChoice=='4'): # inspect target
+            print(Creature1)
+        if (playerChoice=='9'): # Display options
+            print(" 1 to attack '{}'\n 2 to Heal\n 3 to inspect '{}'\n 4 to inspect '{}'\n 9 to show this\n 0 to quit".format(Creature1.name, marvin.name, Creature1.name))
+
+        playerChoice = raw_input('Enter things: ')
+        
+def combatOneOnTwo(Creature1, Creature2):
+    playerChoice = raw_input('Enter things: ')
+    while (playerChoice != '0'): 
+        if (playerChoice=='1'): # attack target 1
+            marvin.attack(Creature1)
+            Creature1.attack(marvin)
+            Creature2.attack(marvin)
+        if (playerChoice=='2'): # attack target 2
+            marvin.attack(Creature2)
+            Creature1.attack(marvin)
+            Creature2.attack(marvin)
+        if (playerChoice=='3'): # Heal self
+            marvin.heal()
+            Creature1.attack(marvin)
+            Creature2.attack(marvin)
+        if (playerChoice=='4'): # inspect self
+            print(marvin)
+        if (playerChoice=='5'): # inspect target 1
+            print(Creature1)
+        if (playerChoice=='6'): # inspect target 2
+            print(Creature2)
+        if (playerChoice=='9'): # Display options
+            print(" 1 to attack '{}'\n 2 to attack '{}'\n 3 to Heal\n 4 to inspect '{}'\n 5 to inspect '{}'\n 6 to inspect '{}'\n 9 to show this\n 0 to quit".format(Creature1.name, Creature2.name, marvin.name, Creature1.name, Creature2.name))
+        playerChoice = raw_input('Enter things: ')
+        
+def combatOneOnThree(Creature1, Creature2, Creature3):
+    playerChoice = raw_input('Enter things: ')
+    while (playerChoice != '0'): 
+        if (playerChoice=='1'): # attack target 1
+            marvin.attack(Creature1)
+            Creature1.attack(marvin)
+            Creature2.attack(marvin)
+            Creature3.attack(marvin)
+        if (playerChoice=='2'): # attack target 2
+            marvin.attack(Creature2)
+            Creature1.attack(marvin)
+            Creature2.attack(marvin)
+            Creature3.attack(marvin)
+        if (playerChoice=='3'): # attack target 3
+            marvin.attack(Creature3)
+            Creature1.attack(marvin)
+            Creature2.attack(marvin)
+            Creature3.attack(marvin)
+        if (playerChoice=='4'): # Heal self
+            marvin.heal()
+            Creature1.attack(marvin)
+            Creature2.attack(marvin)
+            Creature3.attack(marvin)
+        if (playerChoice=='5'): # inspect self
+            print(marvin)
+        if (playerChoice=='6'): # inspect target 1
+            print(Creature1)
+        if (playerChoice=='7'): # inspect target 2
+            print(Creature2)
+        if (playerChoice=='8'): # inspect target 3
+            print(Creature3)
+        if (playerChoice=='9'): # Display options
+            print(" 1 to attack '{}'\n 2 to attack '{}'\n 3 to attack '{}'\n 4 to Heal\n 5 to inspect '{}'\n 6 to inspect '{}'\n 7 to inspect '{}'\n 8 to inspect '{}'\n 0 to quit".format(Creature1.name, Creature2.name, Creature3.name, marvin.name, Creature1.name, Creature2.name, Creature3.name))
+        playerChoice = raw_input('Enter things: ')
 
 jeff = Creature('Jeff', 10, 2, 1)
 alan = Creature('Alan', 10, 3, 1)
-marvin = Marvin('Marvin', 17, 6, 5)
+tim = Creature('Tim', 10, 1, 1)
 
-"""
-combat handler
-"""
-playerChoice = raw_input('Enter things: ')
+combatOneOnOne(alan)
+combatOneOnTwo(jeff, alan)
+combatOneOnThree(jeff, alan, tim)
 
-while (playerChoice != '0'):
-    if (playerChoice=='1'):
-        marvin.attack(jeff)
-        jeff.attack(marvin)
-        alan.attack(marvin)
-    if (playerChoice=='2'):
-        marvin.attack(alan)
-        jeff.attack(marvin)
-        alan.attack(marvin)
-    if (playerChoice=='3'):
-        marvin.heal()
-        jeff.attack(marvin)
-        alan.attack(marvin)
-    playerChoice = raw_input('Enter things: ')
